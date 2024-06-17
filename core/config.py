b731dfcb87d36a02a59f756e933920dda122ee96
awesomex <bestawes0mex@gmail.com>
2024-06-17 13:51:25 +0500

    fix

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_HOST: str
     DB_NAME: str
-    DB_NAME_TEST: str
+    # DB_NAME_TEST: str
     DB_PASS: str
     DB_PORT: int
     DB_USER: str
@@ -16,9 +16,9 @@ class Settings(BaseSettings):
     def get_db_url(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    @property
-    def get_db_url_test(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME_TEST}"
+    # @property
+    # def get_db_url_test(self):
+    #     return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME_TEST}"
 
     model_config = SettingsConfigDict(env_file=".env")
 
